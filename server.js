2ecdfaa2aaaabf7779e629550a58e3c5f97eea5e
Wilson <wchang@oddz.com>
2016-06-25 00:18:03 -0700

    Revert "wip"
    This reverts commit 3c62cb16e3beb02783b25fdf61ace74fd06a6509.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use( route.post('/webhook', webhook) )
 
 function *webhook() {
   req = yield parse(this)
-  this.body = yield req.hub.challenge
+  this.body = req.hub.challenge
 }
 
 app.listen(process.env.PORT || 9000)
