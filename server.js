b655537b0a8b04f8e24c625306cc86b9303c3299
Wilson <wchang@oddz.com>
2016-06-24 23:41:47 -0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+app = require('koa')()
+, route = require('koa-route')
+, parse = require('co-body')
+
+app.use( route.post('/webhook', webhook) )
+
+function *webhook() {
+  req = yield parse(this)
+  this.body = req
+}
+
+app.listen(3000)
